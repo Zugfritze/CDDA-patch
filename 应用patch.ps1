e0e6aa2a8b1bb04c50a0266bc0dfcdb796160fc1
Zugfritze <70323844+Zugfritze@users.noreply.github.com>
2023-09-07 10:43:21 +0800

    增加应用patch的脚本

---
+++ 应用patch.ps1
@@ -0,0 +1,29 @@
+# 获取当前脚本所在目录
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+# 获取当前目录
+$currentDir = Get-Location
+
+# 获取脚本所在目录下的所有 Git Patch 文件
+$patchFiles = Get-ChildItem -Path $scriptPath -Filter "*.patch" | Sort-Object @{Expression = {
+        [regex]::Match($_.BaseName, '^\d+').Value -as [int]
+    }
+}
+
+# 遍历每个 Patch 文件
+foreach ($patchFile in $patchFiles) {
+    $patchFilePath = "$scriptPath\$($patchFile.Name)"
+
+    try {
+        # 应用 Patch 文件到当前目录
+        Write-Host "应用 Patch 文件 $($patchFile.Name) 到当前目录..."
+        git apply $patchFilePath --directory $currentDir
+        Write-Host "应用完成！"
+    }
+    catch {
+        # 应用 Patch 发生错误
+        Write-Host "应用 Patch 文件 $($patchFile.Name) 时出现错误:"
+        Write-Host $_.Exception.Message
+        Write-Host "跳过当前 Patch 文件继续处理下一个文件."
+    }
+}
